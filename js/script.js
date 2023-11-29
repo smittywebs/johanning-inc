d3750d4112e555e4a717833ff8cda568ed03dc0c
Smitty81-webs <82186230+Smitty81-webs@users.noreply.github.com>
2023-11-28 21:01:36 -0600

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const menu = document.getElementById('menu')
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
   btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
-})
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
